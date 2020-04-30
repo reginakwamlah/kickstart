757d6db43b4c145ec8aaf33617db3b0682c4db1e
reginakwamlah <59487822+reginakwamlah@users.noreply.github.com>
2020-04-30 13:59:18 -0500

    Update default.rb

--- kickstart/attributes/default.rb
+++ kickstart/attributes/default.rb
@@ -6,17 +6,17 @@
 
 # DHCP
 
-default['bedrock']['dhcp']['domain-name'] = "100.220.81.170"
-default['bedrock']['dhcp']['domain-name-server'] = "100.220.81.170"
-default['bedrock']['dhcp']['subnet'] = "10.220.81.128"
-default['bedrock']['dhcp']['netmask'] = "255.255.255.192"
-default['bedrock']['dhcp']['routers'] = "10.220.81.191"
-default['bedrock']['dhcp']['range-min'] = "10.220.81.21"
-default['bedrock']['dhcp']['range-max'] = "10.220.81.120"
-default['bedrock']['dhcp']['next-server'] = "10.220.81.191"
+default['bedrock']['dhcp']['domain-name'] = "0.0.0.0"
+default['bedrock']['dhcp']['domain-name-server'] = "0.0.0.0"
+default['bedrock']['dhcp']['subnet'] ="0.0.0.0"
+default['bedrock']['dhcp']['netmask'] = "0.0.0.0"
+default['bedrock']['dhcp']['routers'] = "0.0.0.0"
+default['bedrock']['dhcp']['range-min'] = "0.0.0.0"
+default['bedrock']['dhcp']['range-max'] = "0.0.0.0"
+default['bedrock']['dhcp']['next-server'] = "0.0.0.0"
 default['bedrock']['dhcp']['default-lease-time'] = 3600
 default['bedrock']['dhcp']['max-lease-time'] = 7200;
 default['bedrock']['dhcp']['pxe-filename'] = "/var/lib/netboot/pxelinux.cfg";
-default['bedrock']['dhcp']['broadcast'] = "10.220.81.191"
+default['bedrock']['dhcp']['broadcast'] = "0.0.0.0"
 
 # TFTP
